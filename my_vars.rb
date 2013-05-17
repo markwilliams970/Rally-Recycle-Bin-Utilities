ef3d39921b73108687687387f44e045f2fe1aa7a
markwilliams <markwilliams@markw-macbook-pro.f4tech.com>
2013-05-17 15:44:36 -0600

    User-defined local variables

---
+++ my_vars.rb
@@ -0,0 +1,7 @@
+$my_base_url                     = "https://rally1.rallydev.com/slm"
+$my_username                     = "user@company.com"
+$my_password                     = "topsecret"
+$my_workspace                    = "My Workspace"
+
+# File containing CSV list of items to delete from Recycle Bin
+$filename                        = "recyclebin_todelete.csv"
